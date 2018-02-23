ecee0a888881652163694c06ac9f310b88d40e08
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 18:40:37 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,8 +1,14 @@
 class Hero
+  @@heroes = []
+  
   attr_reader :name, :power, :bio
 
   def initialize(name, power, bio)
     @name, @power, @bio = name, power, bio
+  end
+
+  def self.all 
+    @@heroes
   end
 
 end
