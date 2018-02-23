d8b5fbfea4fc9f9d8acbc1a75d1c1c15d15d4ddb
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 19:24:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(params[:team])
-      binding.pry 
+      binding.pry
       params[:team][:members].each do |member|
         Hero.new(member[:name], member[:power], member[:bio])
       end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,8 @@
 class Team
   attr_reader :name, :motto
 
-  def initialize(name, motto)
-    @name, @motto = name, motto
+  def initialize(params)
+    @name = params[:name] 
+    @motto = params[:motto]
   end
 end
