b326225b650a634b12c2353b3951964465aa2145
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 19:26:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(params[:team])
-      binding.pry
+      
       params[:team][:members].each do |member_info|
         Hero.new(member_info)
       end
